7b70b0ea858eb0614c02df4642ac7185f76ee4bd
SheebuAnsari <38660373+SheebuAnsari@users.noreply.github.com>
2024-09-30 15:45:09 +0530

    Update Configuration.sql
    add su user ie default user

--- MadarsaSchema/Configuration.sql
+++ MadarsaSchema/Configuration.sql
@@ -1,4 +1,7 @@
-use MADARSA123
+use MADARSA
+INSERT INTO tbl_UserRegistration (sUserName, iUserType) VALUES ('su', 1) 
+INSERT INTO tbl_Login (iRegistrationId, sUserId,sUserPassword) values (1, 'su', 'su')
+  
 --START : Enable disable tab and its children menu
 select *from tbl_Menu where bIsGroup=1 
 select *from tbl_Menu where iMenuId  = 400
