9f0feff3651fe140f74291f6de5b3bb6af46a8b2
SheebuAnsari <38660373+SheebuAnsari@users.noreply.github.com>
2024-12-15 22:45:21 +0530

    Update Alters.sql

--- MadarsaSchema/Alters.sql
+++ MadarsaSchema/Alters.sql
@@ -2,4 +2,4 @@ use MadarsaA
 alter table tbl_UserRegistration add sDOBId nvarchar (100)
 
 alter table tbl_Students Drop column sclasssection
-alter table tbl_LicenseInfo  add sDbNameCaption nvarchar (255)
+alter table tbl_LicenseInfo  add sDomainName nvarchar (255)
