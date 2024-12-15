8ff5a4f69bc29f1b5b61876ef6495ec93c33645a
SheebuAnsari <38660373+SheebuAnsari@users.noreply.github.com>
2024-12-15 15:51:58 +0530

    Update CommonQueries.sql

--- MadarsaSchema/CommonQueries.sql
+++ MadarsaSchema/CommonQueries.sql
@@ -17,7 +17,7 @@ update tbl_Menu set bIsActive = 0 where bIsGroup= 1 and iMenuId =0
 --update tbl_Menu set bIsActive = 0 where iSubModule in (select iModule from tbl_Menu where iModule = 0)
 
 --Start : Provide Licence info
---insert into tbl_LicenseInfo (sDbName, sProvider,sKey,iEducationStage,iInstallationDate,iFromDate,iToDate) values ('MadarsaA', 'Sheebu', 'Key-123', 2, 0, 132580097, 132710940)
+--insert into tbl_LicenseInfo (sDbName, sProvider,sKey,iEducationStage,iInstallationDate,iFromDate,iToDate, sDbNameCaption) values ('MadarsaA', 'Sheebu', 'Key-123', 2, 0, 132580097, 132710940, 'MadarsaA')
 --select *from tbl_LicenseInfo
 --End : Provide Licence info
 
